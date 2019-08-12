b6b41dfdcbe28f84049cde141a55b3d7c1579270
neko <469867876@qq.com>
2019-08-12 23:59:30 +0800

    1

---
+++ 001/uniform.frag
@@ -0,0 +1,36 @@
+precision mediump float;
+
+varying vec2 vTexCoord;
+
+uniform float time;
+
+uniform sampler2D cactiTex;
+
+float rand(vec2 n) { 
+	return fract(sin( dot(n, vec2(12.9898, 4.1414)) ) * 43758.5453);
+}
+
+float noise(vec2 p){
+	vec2 ip = floor(p);
+	vec2 u = fract(p);
+	u = u*u*(3.0-2.0*u);
+	
+	float res = mix(
+		mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
+		mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
+	return res*res;
+}
+
+void main() {
+
+  vec2 uv = vTexCoord;
+  uv.y = 1.0 - uv.y;
+
+  float sineWave = sin(noise(uv) * 7.0);
+
+  vec2 distort = vec2(noise(uv * (10.0 - time)), 0.0);
+
+  vec4 tex = texture2D(cactiTex, uv + distort);
+
+  gl_FragColor = tex;
+}
---
+++ 002/sketch.js
@@ -0,0 +1,38 @@
+// this time we're going to send an image to the shader
+// in shaderland, an image is called a "texture"
+// https://p5js.org/reference/#/p5.Shader/setUniform
+
+// a shader variable
+let uniformsShader;
+let cactiImg;
+
+function preload(){
+  // load the shader
+  uniformsShader = loadShader('uniform.vert', 'uniform.frag');
+  cactiImg = loadImage('cacti.jpg');
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(564, 747, WEBGL);
+  noStroke();
+  console.log(cactiImg)
+}
+
+function draw() {  
+  // shader() sets the active shader with our shader
+  shader(uniformsShader);
+
+  uniformsShader.setUniform('time', frameCount * 0.01);
+  // setUniform can also send an image to a shader
+  // 'cactiTex' is the name of the variable in our shader
+  // cactiImg, is a normal p5 image object
+  uniformsShader.setUniform('cactiTex', cactiImg);
+
+  // rect gives us some geometry on the screen
+  rect(0,0,width, height);
+}
+
+function windowResized(){
+  // resizeCanvas(windowWidth, windowHeight);
+}
