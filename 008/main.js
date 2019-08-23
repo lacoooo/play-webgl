dfe549ab478cf7c86b1845457deda47dcf15bb94
Neko <469867876@qq.com>
2019-08-23 10:35:24 +0800

    1

--- 008/main.js
+++ 008/main.js
@@ -11,22 +11,32 @@ import { Material, Geometry } from 'three';
         }
 
         setup() {
-            const material = new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load(require('./img/1.jpg')) } )
+            const material = new THREE.MeshBasicMaterial(
+                { map: new THREE.TextureLoader().load(require('./img/1.jpg'))
+            } )
             material.side = THREE.DoubleSide
             const geometry = new THREE.SphereGeometry(100, 100, 100)
             this.sphere = new THREE.Mesh( geometry, material )
             this.scene.add(this.sphere)
+            setTimeout(() => {
+                this.renderer.render(this.scene, this.camera)
+            }, 500);
         }
 
-        loop() {
-            // this.sphere.rotation.y -= 0.004
-            // this.sphere.rotation.x -= 0.004
-            // this.sphere.rotation.z -= 0.004
-        }
+        // loop() {
+        //     // this.sphere.rotation.y -= 0.04
+        //     // this.sphere.rotation.x -= 0.04
+        //     // this.sphere.rotation.z -= 0.04
+        // }
 
     }
 
-    window.pano = new Pano()
+    const options = {
+        width: 1200,
+        height: 700
+    }
+
+    window.pano = new Pano(options)
     window.THREE = THREE
     pano.camera.position.set(0, 0, 0)
     pano.camera.lookAt(new THREE.Vector3(0, 0, -100))
--- 008/pano.js
+++ 008/pano.js
@@ -2,16 +2,21 @@ import * as THREE from 'three'
 
 class PanoBase {
 
-    constructor() {
+    constructor(options = {}) {
         this.container = null
         this.scene = null
         this.camera = null
         this.renderer = null
         this.frameCount = 0
         this.sphere = null
+        this.options = {
+            width: options.width || window.innerWidth,
+            height: options.height || window.innerHeight
+        } 
 
         this.preSet()
-        this.setup()
+        this.bindScale()
+        this.setupBase()
         this.loopBase()
     }
 
@@ -25,15 +30,26 @@ class PanoBase {
 
         this.scene = new THREE.Scene()
         this.scene.background = new THREE.Color('#000000')
-        this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000)
+        this.camera = new THREE.PerspectiveCamera(90, this.options.width / this.options.height, 1, 10000)
         this.camera.lookAt(this.scene.position)
 
         this.renderer = new THREE.WebGLRenderer({ antialias: true })
         this.renderer.setPixelRatio(window.devicePixelRatio)
-        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.setSize(this.options.width, this.options.height)
         
         this.container.appendChild(this.renderer.domElement)
 
+    }
+
+    bindScale() {
+        this.container.addEventListener('mousewheel', (ev = window.event) => {
+            console.log(ev.wheelDelta)
+        })
+    }
+    setupBase() {
+        this.setup()
+        console.log(1)
+        this.renderer.render(this.scene, this.camera)
     }
 
     setup() {
@@ -43,19 +59,21 @@ class PanoBase {
     loopBase(instance) {
 
         instance = this || instance
+        instance.renderer.render(instance.scene, instance.camera)
         if (instance.__proto__.loop == instance.__proto__.__proto__.loop) {
-            console.log('未设置loop')
-            return
+            if (instance.frameCount === 0) {
+                this.loop()
+            }
+        } else {
+            this.loop()
         }
         instance.frameCount ++
-        instance.renderer.render(instance.scene, instance.camera)
-        instance.loop()
         requestAnimationFrame(instance.loopBase.bind(instance))
 
     }
 
     loop() {
-
+        console.log('未设置loop')
     }
 
 }
