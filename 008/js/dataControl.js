ef9840ddb83a401571f2bf86dafb98ac0c8ed199
Neko <469867876@qq.com>
2019-08-26 18:24:58 +0800

    1

--- 008/js/dataControl.js
+++ 008/js/dataControl.js
@@ -46,6 +46,7 @@ class DataControl {
   }
 
   addNewTag(ele, pano) {
+    if (!pano.canControl) return
     ele.dom = document.createElement('div')
     ele.dom.className = 'pointWrap'
     ele.dom.id = `tag-${this.points.length}`
@@ -73,7 +74,7 @@ class DataControl {
       contentWords.innerHTML = words[Math.floor(Math.random()* words.length)]
     })
 
-    let selectItemName = this.appendDom('div', 'selectItem', typeSelect, '名称')
+    let selectItemName = this.appendDom('div', 'selectItem', typeSelect, '地点名称')
     selectItemName.addEventListener('click', (ev = window.event) => {
       ev.preventDefault()
       let contentName = typeSelect
@@ -82,6 +83,45 @@ class DataControl {
         '马山', '海岸城', '金匮公园', '人民医院', '崇安寺', '鼋头渚', '江南大学', '雪浪山'
       ]
       contentName.innerHTML = names[Math.floor(Math.random()* names.length)]
+    })
+
+    let selectItemArticle = this.appendDom('div', 'selectItem', typeSelect, '图文介绍')
+    selectItemArticle.addEventListener('click', (ev = window.event) => {
+      ev.preventDefault()
+      let contentArticle = typeSelect
+      contentArticle.className = 'contentArticle'
+      let article = `
+        <img class='thumb' src='${require('../img/thumb.jpg')}' />
+        <div class='detail'>
+        马山镇是1992年10月国务院批准成立的国家级旅游度假区，地处无锡西南美丽的马山半岛，总规划面积65平方公里，地域分布在太湖十八湾（12平方公里）、马圩圩区（20平方公里）、马迹山岛（33平方公里），下辖灵山景区管理处、生命科学园区管理处、吴都阖闾城管理处三个管理处和古竹、群丰、嶂青、西村、和平、万丰、栖云、耿湾、阖闾、湖山、峰影、乐山、迎晖13个社区，常住人口3.9万。
+        </div>
+      `
+      contentArticle.innerHTML = article
+    })
+
+    let selectItemVideo = this.appendDom('div', 'selectItem', typeSelect, '视频介绍')
+    selectItemVideo.addEventListener('click', (ev = window.event) => {
+      ev.preventDefault()
+      let contentVideo = typeSelect
+      contentVideo.className = 'contentVideo'
+      let video = `
+        <video class='thumb' controls src='${require('../img/1.mp4')}'></video>
+        <div class='detail'>
+        无锡小笼包，当地人又称小笼馒头，以皮薄卤多而誉遍沪、宁、杭一带，是江苏无锡地方传统名点，已有百年历史。
+        </div>
+      `
+      contentVideo.innerHTML = video
+    })
+
+    let selectItemHref = this.appendDom('div', 'selectItem', typeSelect, '跳转链接')
+    selectItemHref.addEventListener('click', (ev = window.event) => {
+      ev.preventDefault()
+      let contentHref = typeSelect
+      contentHref.className = 'contentHref'
+      let Href = `
+        <a href='https://baike.baidu.com/item/%E6%97%A0%E9%94%A1/135983?fr=aladdin' target='_blank'>无锡</a>
+      `
+      contentHref.innerHTML = Href
     })
     
     let selectItemCancel = this.appendDom('div', 'selectItem', typeSelect, '取消')
